32d90bb30e4f2f6189f6797558cadd468f33671c
C. Alan Zoppa <alan.zoppa@gmail.com>
2015-05-17 17:09:36 -0500

    better user of shared nocks

--- test/hue_getstate_test.js
+++ test/hue_getstate_test.js
@@ -4,19 +4,12 @@ var url = require('url');
 var fake_hue_response = require('./fake_hue_config');
 require('chai').should();
 var nock = require('nock');
-
-function nockHueConfig(settings) {
-    var parsed = url.parse(settings.hue_config_url);
-    var theNock = nock(`${parsed.protocol}\/\/${parsed.host}`)
-        .get(parsed.path)
-        .delay(10)
-        .reply(200, fake_hue_response)
-}
+var sharedNocks = require('./shared_nocks')
 
 describe("Hue Config Creator", function(){
     beforeEach(function(){
         this.hueSettings = new HueSettings(config);
-        nockHueConfig(this.hueSettings);
+        sharedNocks.nockHueConfig(this.hueSettings);
     })
     it("Should get the url", function() {
         this.hueSettings.hue_config_url.should.eql('http://192.168.1.1/api/0000000000000000000000000000000')
--- test/shared_nocks.js
+++ test/shared_nocks.js
@@ -5,11 +5,11 @@ var fake_hue_response = require('./fake_hue_config');
 
 module.exports = {
     nockHueConfig: function (settings) {
-    var parsed = url.parse(settings.hue_config_url);
-    var theNock = nock(`${parsed.protocol}\/\/${parsed.host}`)
-        .get(parsed.path)
-        .delay(10)
-        .reply(200, fake_hue_response)
+        var parsed = url.parse(settings.hue_config_url);
+        var theNock = nock(`${parsed.protocol}\/\/${parsed.host}`)
+            .get(parsed.path)
+            .delay(10)
+            .reply(200, fake_hue_response)
     }, 
 
     nockHueState: function (settings, light, on) {
